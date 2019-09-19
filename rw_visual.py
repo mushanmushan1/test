a8e6dd36235996267d7e500d373619ddd33b8a15
mushanmushan1 <bli@sgg.whu.edu.cn>
2019-09-19 19:24:21 +1000

    This is Version 4 for GitHub

---
+++ rw_visual.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+# 只要程序处于活动状态，就不断模拟随机漫步
+while True:
+    # 创建一个RandomWalk实例，并将其包含的点都绘制出来
+    rw = RandomWalk(5000)
+    rw.fill_walk()
+
+    # 设置窗口尺寸，单位inch
+    plt.figure(figsize=(10, 6))
+
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors="none", s=1)
+    plt.scatter(0, 0, c="green", edgecolors="none", s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c="red", edgecolors="none", s=100)
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n):")
+    if keep_running == "n":
+        break
