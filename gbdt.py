21e27d1ae74e3a4f810d6268c950f78a1aac7d60
mushanmushan1 <bli@sgg.whu.edu.cn>
2019-09-20 22:21:10 +1000

    This is Version 7 for GitHub & Web Crawling

--- gbdt.py
+++ gbdt.py
@@ -1,1 +1,15 @@
-print("GBDT is a machine learning algorithm.")
+import requests
+from bs4 import BeautifulSoup
+
+
+baidu = requests.get("https://www.baidu.com/").content
+# print(baidu)
+
+soup = BeautifulSoup(baidu, "html.parser")
+# print(soup)
+
+links = soup.find_all("a")
+
+for link in links:
+    print(link.string)
+
