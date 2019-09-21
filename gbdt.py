70e2c6ae468b12b9dbf27dda22c4372c3294963e
mushanmushan1 <bli@sgg.whu.edu.cn>
2019-09-21 20:06:18 +1000

    This is Version 9 for GitHub & Web Crawling

--- gbdt.py
+++ gbdt.py
@@ -3,15 +3,14 @@ import requests
 from bs4 import BeautifulSoup
 
 
-baidu = requests.get("https://www.baidu.com/").content
-# print(baidu)
+# data = requests.get("https://www.baidu.com/").content
+data = requests.get("http://www.pythonscraping.com/pages/page1.html").content
 
-soup = BeautifulSoup(baidu, "html.parser")
-print(soup.text)
-# sys.exit(0)
+soup = BeautifulSoup(data, "html.parser")
+print(soup.title.text)
+sys.exit(0)
 
 links = soup.find_all("a")
 
 for link in links:
     print(link.string)
-
