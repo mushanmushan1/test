ba72c7977a5102e00aabd1c159e9d5518e30e492
mushanmushan1 <bli@sgg.whu.edu.cn>
2019-09-22 15:42:21 +1000

    This is Version 11 for GitHub & dataload

--- gbdt.py
+++ gbdt.py
@@ -2,6 +2,7 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
 
 # # data = requests.get("https://www.baidu.com/").content
 # data = requests.get("http://www.pythonscraping.com/pages/page1.html").content
@@ -15,9 +16,28 @@ from urllib.request import urlopen
 # for link in links:
 #     print(link.string)
 
-html = urlopen("http://www.pythonscraping.com/pages/page1.html")
-bsObj = BeautifulSoup(html.read(), "html.parser")
-print(bsObj.h1)
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except (HTTPError, URLError) as e:
+        print("URL is not found")
+        return None
+
+    try:
+        bsObj = BeautifulSoup(html.read(), "html.parser")
+        title = bsObj.h1
+    except AttributeError as e:
+        return None
+
+    return title
+
+
+title = getTitle("http://www.pythonscraping.com/pages/page1.html")
+if title is None:
+    print("Title could not be found")
+else:
+    print(title.text)
 
 
 
