fb4891c29661a69dd61f876402fcfd9db4b68155
mushanmushan1 <bli@sgg.whu.edu.cn>
2019-09-23 12:30:06 +1000

    :art: this is 10 version

--- gbdt.py
+++ gbdt.py
@@ -1,11 +1,45 @@
 import sys
+import re       # 正则表达
+import random
+import datetime
 import requests
 from bs4 import BeautifulSoup
+from scrapy import Item, Field
 from urllib.request import urlopen
 from urllib.error import HTTPError, URLError
 
 
-def getlinks(url):
+# def getlinks(url):
+#     try:
+#         html = urlopen(url)
+#     except (HTTPError, URLError) as e:
+#         print("URL is not found")
+#         return None
+#
+#     try:
+#         bsObj = BeautifulSoup(html, "html.parser")
+#
+#         # 获取 web页面链接
+#         links = bsObj.find("div", {"id": "bodyContent"}).findAll("a", href=re.compile("^(/wiki/)((?!:).)*$"))
+#     except AttributeError as e:
+#         print("bsObj fails")
+#         return None
+#
+#     return links
+#
+# links = getlinks("https://en.wikipedia.org/wiki/Kevin_Bacon")
+#
+# if links == None:
+#     print("Links could not be found")
+# else:
+#     for link in links:
+#         if "href" in link.attrs:
+#             print(link.attrs['href'])
+
+random.seed(datetime.datetime.now())
+
+
+def getLinks(url):
     try:
         html = urlopen(url)
     except (HTTPError, URLError) as e:
@@ -14,23 +48,21 @@ def getlinks(url):
 
     try:
         bsObj = BeautifulSoup(html, "html.parser")
-        links = bsObj.find_all("a")  # 获取 web页面链接
-
+        links = bsObj.find("div", {"id": "bodyContent"}).findAll("a", href=re.compile("^(/wiki/)((?!:).)*$"))
     except AttributeError as e:
-        print("bsObj fails")
+        print("bsObj fails!")
         return None
 
     return links
 
 
-links = getlinks("https://en.wikipedia.org/wiki/Kevin_Bacon")
+links = getLinks("https://en.wikipedia.org/wiki/Kevin_Bacon")
 
-if links == None:
-    print("Links could not be found")
-else:
-    for link in links:
-        if "href" in link.attrs:
-            print(link.attrs['href'])
+while len(links) > 0:
+    newArticle = links[random.randint(0, len(links)-1)].attrs["href"]
+    print(newArticle)
+
+    links = getLinks(newArticle)
 
 
 
